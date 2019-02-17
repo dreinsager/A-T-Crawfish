ac0e29e90b74a5f47bc9c161140de30b11f62bd1
dreinsager <dreinsager@gmail.com>
2019-02-17 16:20:35 -0600

    database success with errors

--- errors.php
+++ errors.php
@@ -12,4 +12,61 @@ if (empty($username) || empty($password) || empty($passrepeat)) {
     exit();
 
 }
+else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+    header("Location: registeradmin.php?error=invaliduname");
+    exit();
+
+}
+else if ($password !== $passrepeat) {
+    header("Location: registeradmin.php?error=pwdcheck&uname=".$username);
+    exit();
+
+}
+else {
+    //Prepared statement for a more secure way to protect sql being put into the text fields
+    $sql = "SELECT uname FROM admin WHERE uname=? ";
+    $stmt = mysqli_stmt_init($mysqli);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: registeradmin.php?error=sqlerror");
+        exit();
+        }
+    else {
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $checkresult = mysqli_stmt_num_rows($stmt);
+
+        if ($checkresult > 0) {
+            header("Location: registeradmin.php?error=useralreadyexists");
+            exit();
+            }
+        else {
+            //
+            $sql = "INSERT INTO admin (uname, pwd) VALUES (?, ?)";
+            $stmt = mysqli_stmt_init($mysqli);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                header("Location: registeradmin.php?error=sqlerror");
+                exit();
+                }
+            else {
+                //Hashing the password using decrypt.. automatically updates
+                $pwdhash = password_hash($password, PASSWORD_DEFAULT);
+
+                mysqli_stmt_bind_param($stmt, "ss", $username, $pwdhash);
+                mysqli_stmt_execute($stmt);
+                header("Location: registeradmin.php?register=success");
+                exit();
+
+            }
+        
+        }
     }
+}
+mysqli_stmt_close($stmt);
+mysqli_close($mysqli);
+}
+else {
+    header("Location: registeradmin.php");
+    exit();
+
+}
