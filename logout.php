73d1f197680895176c80983db91833bdb2e29f3e
dreinsager <dreinsager@gmail.com>
2019-02-21 16:33:00 -0600

    updated logout

--- logout.php
+++ logout.php
@@ -3,7 +3,6 @@
 session_start();
 session_unset();
 session_destroy();
-echo '<p>You have successfully logged out!';
-
-// header("Location: adminlogin.php");
+echo '<p>You have successfully logged out!<br><br>';
+echo'<a href="adminlogin.php">Click Here</a> to return to Login page';
 ?>
