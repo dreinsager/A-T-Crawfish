1b5fe97c3111ea5e579de47a79911aa2040e453c
dreinsager <dreinsager@gmail.com>
2019-03-25 21:51:55 -0500

    styling customer

--- Customer/custindex.php
+++ Customer/custindex.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <?php require '../../DB/connect.php' ?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Welcome to A&T Crawfish</title>
@@ -10,6 +11,21 @@
     <script src="main.js"></script>
 </head>
 <body>
-    
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+<img src="../img/logo.jpg" width="50" height="50" class="d-inline-block align-top" alt="">&nbsp;
+  <a class="navbar-brand" href="#">Welcome to A&T Crawfish</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Gallery</a>
+      <a class="nav-item nav-link" href="#">Menu</a>
+      <a class="nav-item nav-link" href="#">Order</a>
+
+    </div>
+  </div>
+</nav>
 </body>
 </html>
