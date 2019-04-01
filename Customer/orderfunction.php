137950d7336b64ea233b3bae8e0accf3dc59dbd6
dreinsager <dreinsager@gmail.com>
2019-04-01 15:52:29 -0500

    order works

--- Customer/orderfunction.php
+++ Customer/orderfunction.php
@@ -6,20 +6,22 @@
         $phone = $_POST['phone'];
         $email = $_POST['email'];
         $qty = $_POST['qty'];
+        $item = $_POST['item'];
+
 
 
         $_SESSION['message'] = "Order Successfully Added!";
         $_SESSION['msg_type'] = "success";
 
-        $sql = "INSERT INTO orders (fname, lname, phone, email, qty) VALUES (?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO orders (fname, lname, phone, email, qty, item) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = mysqli_stmt_init($mysqli);
-        if(empty($fname) || empty($lname) || empty($phone) || empty($email) || empty($qty)) {
+        if(empty($fname) || empty($lname) || empty($phone) || empty($email) || empty($qty) || empty($item)) {
             echo 'Please enter all fields!';
             header("Location: order.php?error=emptyfields");
             exit();
         }
         if (mysqli_stmt_prepare($stmt, $sql)) {
-            mysqli_stmt_bind_param($stmt, "sssss", $fname, $lname, $phone, $email, $qty);
+            mysqli_stmt_bind_param($stmt, "ssssss", $fname, $lname, $phone, $email, $qty, $item);
             mysqli_stmt_execute($stmt);
 
             header("Location: order.php");
